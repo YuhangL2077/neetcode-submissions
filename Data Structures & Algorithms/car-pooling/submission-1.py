88b7267fd1f9f9e0f863aa250260d6a09381aa91
Yuhang Li <46491765+YuhangL2077@users.noreply.github.com>
2026-09-18 23:57:27 -0700

    Add: car-pooling - submission-1

---
+++ Data Structures & Algorithms/car-pooling/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        events = []
+        for num, i, j in trips:
+            events.append((i, num))
+            events.append((j, -num))
+
+        # events.sort(key=lambda x:x[0])
+        events.sort() # must get off car first, and get on car after
+        curr_capacity = 0
+        max_capacity = 0
+        for event in events:
+            curr_capacity += event[1]
+            max_capacity = max(max_capacity, curr_capacity)
+
+        return capacity >= max_capacity
